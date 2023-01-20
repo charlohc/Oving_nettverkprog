6e05ff1b9a03573287e1e3df954c39843994b1a3
charlohc <Charlottee.Christiansen@gmail.com>
2023-01-20 14:28:46 +0100

    only unique elements

--- main.cpp
+++ main.cpp
@@ -1,47 +1,96 @@
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <algorithm>
+
+//TODO: dårlig fordeling av tall, burde fordelt slik at alle fikk et av laveste tallene så et mer osv..
+
 using namespace std;
-class MyClass {
-    pthread_mutex_t  mutex;
-    int mails;
 
-    void* routine() {
-        for (int i = 0; i < 10; i++) {
-            pthread_mutex_lock(&mutex);
-            mails++;
-            pthread_mutex_unlock(&mutex);
+bool isPrime(int);
+int workload(int,int,int);
+
+void foo(int start, int end, vector<int>*primes, mutex *prime_mutex) {
+
+    for (int j = start; j <= end; j++) {
+
+            if(isPrime(j)) {
+                prime_mutex->lock();
+                primes->push_back(j);
+                prime_mutex->unlock();
+            }
         }
     }
 
-public:
-    int createThreads(int threadNum) {
-        pthread_t th[threadNum];
-        pthread_mutex_init(&mutex, NULL);
-        for (int i = 0; i < threadNum; ++i) {
-            if (pthread_create(th + i, NULL, &MyClass::routine, NULL)!= 0) {
-                perror("failed to create thread");
-                return 1;
-            }
-            printf("thread %d has started\n", i);
-            if (pthread_join(th[i], NULL) != 0) {
-                return 2;
-            }
-            printf("Thread %d has finished\n", i);
+
+    bool isPrime(int n) {
+        if (n == 0 || n == 1) {
+            return false;
         }
-        pthread_mutex_destroy(&mutex);
-        printf("number of mails: %d\n", mails);
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-};
-    int main() {
+
+    void createThreads(int startNum, int endNum, int threadNum, int workPerThread)  {
+        vector<thread> threads(threadNum);
+        vector<int> primes;
+        mutex prime_mutex;
+
+        for (int i = 1; i <= threadNum; i++) {
+            //cout << "start " << startNum << endl;
+            //cout << "end " << endNum << endl;
+            threads.emplace_back(foo, startNum, endNum, &primes, &prime_mutex);
+            startNum = (startNum + workPerThread);
+            endNum = (endNum + workPerThread);
+
+        }
+
+        for(auto& th : threads){
+            if (th.joinable())
+            th.join();
+        }
+
+        vector<int>::iterator it;
+        it = unique(primes.begin(), primes.end());
+
+        primes.resize(distance(primes.begin(),it));
+        sort(primes.begin(), primes.end());
+
+        for (int prime : primes) {
+            cout << prime << ", ";
+        }
+
+        cout << endl;
+    }
+
+
+
+    int workload(int startNumber, int endNumber, int threadNumber) {
+        int  workPerThread = (endNumber - startNumber) / threadNumber;
+        return workPerThread;
+    }
+
+
+int main() {
         std::cout << "Prime number finder!" << std::endl;
         int startNum, endNum, threadNum;
-        cout << "Type in the start number of the interval: "; // Type a number and press enter
-        cin >> startNum; // Get user input from the keyboard
+        cout << "Type in the start number of the interval: ";
+        cin >> startNum;
         cout << "Type in the end number of the interval: ";
         cin >> endNum;
         cout << "Type in the number of threads: ";
         cin >> threadNum;
 
-        MyClass obj;
-        obj.createThreads(threadNum);
+        int workPerThread = workload(startNum, endNum, threadNum);
+
+    createThreads(startNum,(startNum + workPerThread),threadNum, workPerThread);
         return 0;
     }
